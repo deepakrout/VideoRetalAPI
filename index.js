a9e9c5805a30a01d509f7e3d34913ac24e7f1e8a
Deepak Rout <deepakrout@gmail.com>
2019-12-25 14:28:52 -0500

    Role based access done

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
-const mongoose = require('mongoose');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const config = require('config');
-const express = require('express');
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+const mongoose = require("mongoose");
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
+const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
+const config = require("config");
+const express = require("express");
 const app = express();
 
 if (!config.get("jwtPrivateKey")) {
-    console.log('FATAL ERROR: jwtPrivateKey is not defined.');
+    console.log("FATAL ERROR: jwtPrivateKey is not defined.");
     process.exit(1);
 }
 
-mongoose.connect('XXXXXX://XXXXXXXX.<YOURSECRET>.artgdbfschjhsafh/3yt2yt2')
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...'));
+if (!config.get("dbConnection")) {
+    console.log("FATAL ERROR: dbConnection not defined.");
+    process.exit(1);
+}
+
+mongoose
+    .connect(config.get("dbConnection"))
+    .then(() => console.log("Connected to MongoDB..."))
+    .catch(err => console.error("Could not connect to MongoDB..."));
 
 app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
+app.use("/api/genres", genres);
+app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- routes/genres.js
+++ routes/genres.js
@@ -2,11 +2,16 @@ const { Genre, validate } = require('../models/genre');
 const mongoose = require('mongoose');
 const express = require('express');
 const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
 const router = express.Router();
 
 router.get('/', async(req, res) => {
-    const genres = await Genre.find().sort('name');
-    res.send(genres);
+    try {
+        const genres = await Genre.find().sort('name');
+        res.send(genres);
+    } catch (ex) {
+        res.status(500).send('Failed retrieve records. ');
+    }
 });
 
 router.post('/', auth, async(req, res) => {
@@ -32,7 +37,7 @@ router.put('/:id', async(req, res) => {
     res.send(genre);
 });
 
-router.delete('/:id', async(req, res) => {
+router.delete('/:id', [auth, admin], async(req, res) => {
     const genre = await Genre.findByIdAndRemove(req.params.id);
 
     if (!genre) return res.status(404).send('The genre with the given ID was not found.');
